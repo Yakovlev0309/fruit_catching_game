60d9dea606f1245ea9c2db847655ab607f66f9b2
Yakovlev0309 <yakovlev.universe@gmail.com>
2024-05-03 11:26:23 +0300

    Поворот игрока в сторону движения

--- player.cpp
+++ player.cpp
@@ -18,14 +18,14 @@ void Player::keyPressEvent(QKeyEvent *event)
     switch (event->key())
     {
     case Qt::Key_Left:
-        if (pos().x() + image_size.width() / 2 - image_size.width() / 2 > 0)
+        if (pos().x() - image_size.width() / 2 > 0)
         {
             setPos(x() - 10, y());
             flipImage(PlayerDirection::LEFT);
         }
         break;
     case Qt::Key_Right:
-        if (pos().x() + image_size.width() / 2 + image_size.width() / 2 < scene()->width())
+        if (pos().x() + image_size.width() / 2 < scene()->width())
         {
             setPos(x() + 10, y());
             flipImage(PlayerDirection::RIGHT);
@@ -38,11 +38,11 @@ void Player::flipImage(const PlayerDirection &direction)
 {
     if (direction != current_direction)
     {
-        // QTransform transform;
-        // transform.scale((int)direction, 1);
-        // setTransformOriginPoint(0, 0);
-        // setTransform(transform);
-        // setPos(QPointF(pos().x() + image_size.width() * - (int)direction, pos().y()));
+        QTransform transform;
+        transform.scale((int)direction, 1);
+        setTransformOriginPoint(0, 0);
+        setTransform(transform);
+        setPos(QPointF(pos().x() + image_size.width() * - (int)direction, pos().y()));
         current_direction = direction;
     }
 }
